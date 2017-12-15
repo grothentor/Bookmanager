6bca375d20037078c7aa45024479ca724a6e45ea
deema <deemaaikido@gmail.com>
2017-12-15 11:57:58 +0200

    client part: api functions

--- src/main/js/api/BooksAPI.js
+++ src/main/js/api/BooksAPI.js
@@ -1,9 +1,22 @@
 import Base from './BaseAPI';
 
-export default class UserAPI extends Base {
+export default class BooksAPI extends Base {
 
     create(data) {
-        return this.apiClient.post('books',data);
+        return this.apiClient.post('books', data);
     }
+
+    delete(data) {
+        return this.apiClient.delete('books', data);
+    }
+
+    update(data) {
+        return this.apiClient.put('books', data);
+    }
+
+    get(data) {
+        return this.apiClient.get('books', data);
+    }
+
 }
 
